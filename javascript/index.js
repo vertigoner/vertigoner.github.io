878f1e6d9fccae3d90d50578c8e26b2001308c72
vertigoner <nbroberts97@gmail.com>
2018-09-27 00:32:36 -0400

    added some text functionality

--- javascript/index.js
+++ javascript/index.js
@@ -7,6 +7,7 @@
 
 const PI = Math.PI;
 const MARGIN = 20;
+const TEXT_OFFSET = 3;
 const W = (window.innerWidth
 || document.documentElement.clientWidth
 || document.body.clientWidth) - MARGIN;
@@ -14,9 +15,16 @@ const H = (window.innerHeight
 || document.documentElement.clientHeight
 || document.body.clientHeight) - MARGIN;
 
-let horzBaseLen = W / 3;
-let vertBaseLen = W / 3;
-let cornerBaseLen = W / 3;
+const pageLinks = {
+  "Inspired by Sol Lewitt - Wall Drawing 273": "url",
+  "Noah Roberts": "url",
+  "About Me": "url",
+  "Projects": "url"
+}
+
+let horzBaseLen = Math.max(W, H) / 3;
+let vertBaseLen = Math.max(W, H) / 3;
+let cornerBaseLen = Math.max(W, H) / 3;
 let numLines = 10;
 let cluster = {
   left: new LineCluster(1, H / 2, 3 * PI / 2, PI / 2, horzBaseLen, numLines, LINE_CLASS.RED),
@@ -38,8 +46,37 @@ function initSVG(svg) {
   svg.setAttribute("height", H);
 
   for (let key of Object.keys(cluster)) {
+    let clusterGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    clusterGroup.setAttribute("class", "cluster");
+    clusterGroup.setAttribute("id", key);
+
     for (let line of cluster[key].genSvgLines()) {
-      svg.appendChild(line);
+      clusterGroup.appendChild(line);
     }
+
+    svg.appendChild(clusterGroup);
   }
+
+  let linkGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  linkGroup.setAttribute("class", "pageLinks");
+
+  let textDims = cluster.left.textDims.concat(cluster.right.textDims);
+  for (let key of Object.keys(pageLinks)) {
+    let rand = Math.floor(Math.random() * textDims.length);
+    console.log("" + textDims[rand]);
+
+    let x, y;
+
+    if (rand < textDims.length / 2) {
+      x = cluster.left.cx;
+      y = cluster.left.cy;
+    } else {
+      x = cluster.right.cx;
+      y = cluster.right.cy;
+    }
+
+    linkGroup.appendChild(genSvgText(key, x, y, textDims[rand].d, TEXT_OFFSET, textDims[rand].theta));
+  }
+
+  svg.appendChild(linkGroup);
 }
--- javascript/line.js
+++ javascript/line.js
@@ -22,6 +22,7 @@ class LineCluster {
     this.numLines = numLines;
     this.lineClass = lineClass;
     this.dTheta = (Math.abs(rad2 - rad1) - (Math.abs(rad2 - rad1) / numLines)) / numLines;
+    this.textDims = [];
   }
 
   genSvgLines() {
@@ -31,22 +32,40 @@ class LineCluster {
     for (let theta = thetaI; lines.length <= this.numLines; theta = (theta + this.dTheta) % (2 * Math.PI)) {
       let randTheta = theta + Math.random() * (this.dTheta * this.numLines / 10) - (this.dTheta * this.numLines / 20);
       let randLen = this.baseLen * (Math.random() + 0.5);
-      lines.push(genSvgLine(this.cx, this.cy, this.cx + randLen * Math.cos(randTheta),
-        this.cy - randLen * Math.sin(randTheta), this.lineClass));
+      let x = this.cx + randLen * Math.cos(randTheta);
+      let y = this.cy - randLen * Math.sin(randTheta);
+      lines.push(genSvgLine(this.cx, this.cy, x, y, this.lineClass));
+
+      if (lines.length % Math.floor(this.numLines / 3) == 0) {
+        this.storeTextDims(theta, randLen);
+      }
     }
 
     return lines;
   }
 
+  storeTextDims(thetaVal, dVal) {
+      this.textDims.push({theta: thetaVal, d: dVal});
+  }
 }
 
 function genSvgLine(x1, y1, x2, y2, lineClass=LINE_CLASS.RED) {
-  let svgLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  let svgLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
   setAttributes(svgLine, {"x1": x1.toString(), "y1": y1.toString(),
   "x2": x2.toString(), "y2": y2.toString(), "class": lineClass});
 
   return svgLine;
+}
+
+function genSvgText(text, x, y, dx, dy, theta) {
+  let svgText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+  setAttributes(svgText, {"x": x.toString(), "y": y.toString(), "dx": dx.toString(),
+    "dy": dy.toString(), "transform":"rotate(" + theta + ")"});
+  svgText.innerHTML = text;
+
+  return svgText;
 }
 
 function setAttributes(el, attrs) {
